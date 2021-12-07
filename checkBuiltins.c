1b58409f4311ef85dd162402dffc79d71fe05a6d
cocofox1902 <colas.renard1902@gmail.com>
2021-12-07 18:10:13 +0000

    beug resolved

--- checkBuiltins.c
+++ checkBuiltins.c
@@ -28,15 +28,16 @@ int checkBuiltins(char **av, char *buff)
 			_cd(av);
 			return (0);
 		}
-		if (_strcmp("help", av[0]) == 0 && _strlen(av[0]) == 4)
+		else if (_strcmp("help", av[0]) == 0 && _strlen(av[0]) == 4)
 		{
 			_help(av);
+			return (0);
 		}
-		if (_strcmp("echo", av[0]) == 0 && _strlen(av[0]) == 4)
+		else if (_strcmp("echo", av[0]) == 0 && _strlen(av[0]) == 4)
 		{
 			_echo(av);
+			return (0);
 		}
-		return (0);
 	}
 	return (1);
 }
