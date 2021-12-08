32c8f5953e9bdc9328b481afd8193bcc4335e5fe
WILLOT Philippe <willot.ph@gmail.com>
2021-12-08 11:37:55 +0000

    Reverting back code

--- shell_loop.c
+++ shell_loop.c
@@ -15,7 +15,7 @@ int shell_loop(char **argv, int count)
 	if (result == -1)
 	{
 		free_pointer(1, buff);
-		return (3);
+		return (1);
 	}
 	av = fillarguments(buff, " \t");
 	if (av[0] == NULL)
@@ -29,32 +29,25 @@ int shell_loop(char **argv, int count)
 		if (retour == 0 || retour == 2)
 		{
 			free_pointer(1, buff), free_arrayofpointer(av);
-			retour == 0 ? (retour = 0) : (retour = 3);
+			retour == 0 ? (retour = 0) : (retour = 1);
 			return (retour);
 		}
 		if (!(findinthepath(av)))
 		{
 			printerror(argv, count, av), free_pointer(1, buff), free_arrayofpointer(av);
-			return (errno);
+			return (2);
 		}
 		else
 		{
-			if (processus(argv, av, buff, count) != 0)
-			{
-				free_pointer(2, *av, buff), free_arrayofpointer(av);
-				return (errno);
-			}
-			else
-			{
-				free_pointer(2, *av, buff), free_arrayofpointer(av);
-				return (0);
-			}
+			processus(argv, av, buff, count);
+			free_pointer(2, *av, buff), free_arrayofpointer(av);
+			return (0);
 		}
 	}
 	if (processus(argv, av, buff, count) != 0)
 	{
 		free_pointer(1, buff), free_arrayofpointer(av);
-		return (errno);
+		return (2);
 	}
 	free_pointer(1, buff), free_arrayofpointer(av);
 	return (0);
