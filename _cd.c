85f5b92da9da72dec72637b91389cc40a8a52e83
cocofox1902 <colas.renard1902@gmail.com>
2021-12-08 14:55:16 +0000

    cd test check

--- _cd.c
+++ _cd.c
@@ -16,24 +16,29 @@ int _cd(char **av)
 
 	if (av[1] == NULL || _strcmp(av[1], tylde) == 0 || _strcmp(av[1], home) == 0)
 	{
+		chdir(_getenv("HOME"));
 		setenv("OLDPWD", _getenv("PWD"), 1);
-		chdir("/home/vagrant");
 		setenv("PWD", getcwd(buff, sizeof(char) * 1024), 1);
 	}
 	else if (_strcmp(av[1], hyphen) == 0)
 	{
+		if (!_getenv("OLDPWD"))
+		{
+			setenv("OLDPWD", _getenv("PWD"), 1);
+		}
+		else
+		{
+			chdir(_getenv("OLDPWD"));
+			setenv("OLDPWD", _getenv("PWD"), 1);
+			setenv("PWD", getcwd(buff, sizeof(char) * 1024), 1);
+		}
 		write(1, _getenv("OLDPWD"), _strlen(_getenv("OLDPWD")));
 		_printf("\n");
-		chdir(_getenv("OLDPWD"));
-		setenv("OLDPWD", _getenv("PWD"), 1);
-		setenv("PWD", getcwd(buff, sizeof(char) * 1024), 1);
 	}
 	else if (av[1] != NULL && av[1] != hyphen)
 	{
 		if (access(av[1], F_OK) == -1)
-		{
 			write(1, "non\n", 4);
-		}
 		else
 		{
 			setenv("OLDPWD", _getenv("PWD"), 1);
