9097006ab3658206e2a4a00428be07d220f6c8b6
WILLOT Philippe <willot.ph@gmail.com>
2021-12-08 10:03:41 +0000

    Trying to fork the main loop to not break the shell

--- main.c
+++ main.c
@@ -7,14 +7,29 @@
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
-	int count = 0, retour;
+	int count = 0, retour, status;
+	pid_t id;
 
 	signal(SIGINT, handler);
 	while (1)
 	{
 		retour = shell_loop(argv, count);
 		if (retour == 2)
-			return (127);
+		{
+			id = fork();
+			if (id == -1)
+			{
+				perror("Error fork: ");
+				return (EXIT_FAILURE);
+			}
+			if (id != 0)
+			{
+				wait(&status);
+				return (127);
+			}
+			else
+				continue;
+		}
 		if (retour != 0)
 			break;
 		count++;
